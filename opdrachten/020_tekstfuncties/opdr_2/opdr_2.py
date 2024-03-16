94d2add169252408b547ba2e5eaa568c4d14874f
nielsiej96 <97027291@st.deltion.nl>
2024-03-16 16:00:39 +0100

    modified:   opdrachten/020_tekstfuncties/opdr_2/opdr_2.py

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,15 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Niels Brink
 # Groep:
 
 # Hier komt je code...
 
-my_list = ...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"  
 
-print()
+aantal_t = my_string.count("t")
+
+#Zorg ervoor dat de volgende zin op het scherm verschijnt:
+
+#> De letter "t" komt 10x voor in my_string
+
+print(f"> De letter \"t\" komt {aantal_t}x voor in my_string")
