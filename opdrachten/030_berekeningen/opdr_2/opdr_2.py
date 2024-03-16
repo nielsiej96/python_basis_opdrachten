025c82acfeb707e66d5553fbc841894abe351c9d
nielsiej96 <97027291@st.deltion.nl>
2024-03-16 17:12:35 +0100

    modified:   opdrachten/030_berekeningen/opdr_2/opdr_2.py

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,28 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+# Eerste set variabelen
+c1 = -12
+f1 = 102
 
-print()
+# Conversie van Celsius naar Fahrenheit
+fahrenheit1 = c1 * 9/5 + 32
+
+# Conversie van Fahrenheit naar Celsius
+celsius1 = (f1 - 32) * 5/9
+
+# Tweede set variabelen
+c2 = 62.2
+f2 = 96
+
+# Conversie van Celsius naar Fahrenheit
+fahrenheit2 = c2 * 9/5 + 32
+
+# Conversie van Fahrenheit naar Celsius
+celsius2 = (f2 - 32) * 5/9
+
+# Print de bijbehorende output
+print(f"{c1} graden Celsius is gelijk aan {fahrenheit1:.1f} graden Fahrenheit")
+print(f"{f1} graden Fahrenheit is gelijk aan {celsius1:.1f} graden Celsius")
+print(f"{c2} graden Celsius is gelijk aan {fahrenheit2:.0f} graden Fahrenheit")
+print(f"{f2} graden Fahrenheit is gelijk aan {celsius2:.1f} graden Celsius")
