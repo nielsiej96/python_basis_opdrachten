9e55b3a227f6699f52383f13580f4dc61fee33a0
nielsiej96 <97027291@st.deltion.nl>
2024-03-16 15:04:47 +0100

    modified:   opdrachten/010_variabelen/opdr_2/opdr_2.py

--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,15 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Niels Brink
 # Groep:
 
 # Hier komt je code...
 
-my_tuple = ...
+#Een tuple lijkt op een list, maar je kunt hem niet aanpassen. In een list kun je gerust andere waarden toevoegen of verwijderen. Een tuple is zoals die is en verandert verder niet. Dit is een voorbeeld (1, 8, 75, "lekker weertje")
+#11   
+#"Oh een tuple"   
+#13.5   
+#8009  
+
+my_tuple = (11, "Oh een tuple", 13.5, 8009)
+
+print(my_tuple)
