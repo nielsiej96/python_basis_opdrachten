f0ddc151f95a311e4ba90aa52ad08edaff689c7c
nielsiej96 <97027291@st.deltion.nl>
2024-03-16 15:14:55 +0100

    modified:   opdrachten/010_variabelen/opdr_3/opdr_3.py

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -1,9 +1,21 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Niels Brink
 # Groep:
+
+#Een dictionary is ook weer een soort list, maar nu kun je alle waarden in de dictionary ook een naam geven.
+#my_dictionary = {"naam": "pipo", "mijn_getal": 8, "fav_kleur": "groen"}
+
+#naam = willem  
+#achternaam = van der broek  
+#leeftijd = 23  
+#favoriete kleur = blauw  
 
 # Hier komt je code...
 
-my_dict = ...
-
-print()
+my_dict ={
+    "naam": "Willem",
+    "achternaam": "van der Broek",
+    "leeftijd": 23,
+    "favoriete_kleur": "blauw",
+} 
+print(my_dict)
