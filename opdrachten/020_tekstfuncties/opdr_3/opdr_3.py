1f47448ca7d3d9daaa6a2ca3c7c86a53179af9b2
nielsiej96 <97027291@st.deltion.nl>
2024-03-16 16:04:12 +0100

    modified:   opdrachten/020_tekstfuncties/opdr_3/opdr_3.py

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+for _ in range(5):
+    print("    *")
+    print("   ***")
+    print("  ******")
+    print(" ********")
+    print("***********")
+    print("    ***")
+    print("    ***")
+    print("    ***")
+    print()  # Voeg een lege regel toe tussen de bomen
