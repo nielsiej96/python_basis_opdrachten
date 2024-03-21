04e9b020d2172497c40d8fe4a3345cef441eebdc
nielsiej96 <97027291@st.deltion.nl>
2024-03-21 13:19:53 +0100

    modified:   opdrachten/030_berekeningen/opdr_3/opdr_3.py

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,20 @@
 # Groep:
 
 # Hier komt je code...
+
+# Definieer de variabele x
 x = 1
-y = ...
-print( ... )
+
+# Bereken de waarde van y
+y = 4 * x**3 - 2 * x**2 - 1
+
+# Geef de uitkomst weer
+print("De uitkomst is:", y)
 
 x = 2
-y = ...
-print( ... )
+y = 4 * x**3 - 2 * x**2 - 1
+print("De uitkomst is:", y)
 
 x = 0
-y = ...
-print( ... )
+y = 4 * x**3 - 2 * x**2 - 1
+print("De uitkomst is:", y)
