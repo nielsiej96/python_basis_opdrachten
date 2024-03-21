77fc4ea5251f13230529c734da76ffa3263a4234
nielsiej96 <97027291@st.deltion.nl>
2024-03-21 14:36:24 +0100

    modified:   opdrachten/060_loops/opdr_1/opdr_1.py

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -2,8 +2,12 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+# Initialiseer een lege lijst om de getallen op te slaan
+getallen = []
 
-# Hier start de for-loop....
+# Vul de lijst met getallen van 1 tot en met 10
+for i in range(1, 11):
+    getallen.append(i)
 
-my_list = []
+# Print de lijst op het scherm
+print(getallen)
