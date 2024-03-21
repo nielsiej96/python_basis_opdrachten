b9dfa5a2e5227f23d0e1c625dca09b580dcbb687
nielsiej96 <97027291@st.deltion.nl>
2024-03-21 14:41:57 +0100

    modified:   opdrachten/060_loops/opdr_5/opdr_4.py

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,25 @@
 
 # Hier start de for-loop
 
-my_list = []
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+# Sorteer de lijst op alfabet
+pizza_lijst.sort()
+print(pizza_lijst)
+
+# Voeg een pizza toe
+pizza_lijst.append('yo-favorito')
+print(pizza_lijst)
+
+# Verwijder de minst lekkere pizza
+pizza_lijst.remove('olivio')
+print(pizza_lijst)
+
+# Print de eerste 3 pizza's
+print(pizza_lijst[:3])
+
+# Print de middelste pizza
+print(pizza_lijst[len(pizza_lijst)//2])
+
+# Print de laatste 3 pizza's
+print(pizza_lijst[-3:])
