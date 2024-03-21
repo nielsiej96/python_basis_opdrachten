d58e45fb2bb7af2dec16339d1a39cd2229ff1dda
nielsiej96 <97027291@st.deltion.nl>
2024-03-21 14:37:40 +0100

    modified:   opdrachten/060_loops/opdr_2/opdr_2.py

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -6,5 +6,14 @@
 
 # Hier start de for-loop
 
+# Initialiseer een lege lijst
 my_list = []
+
+# Gebruik een for-loop en range om getallen toe te voegen aan de lijst
+for i in range(1, 6):
+    my_list.append(i * 10)
+
+# Print de lijst op het scherm
+print(my_list)
+
 
