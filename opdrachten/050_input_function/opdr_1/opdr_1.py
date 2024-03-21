bf8dcef3ab66f683ef9a69ec50d63cf82893f8e1
nielsiej96 <97027291@st.deltion.nl>
2024-03-21 14:16:53 +0100

    modified:   opdrachten/050_input_function/opdr_1/opdr_1.py

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,15 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+# Vraag om de lengte van de eerste zijde
+zijde1 = float(input("Geef de lengte van de eerste zijde: "))
+
+# Vraag om de lengte van de tweede zijde
+zijde2 = float(input("Geef de lengte van de tweede zijde: "))
+
+# Bereken de lengte van de derde zijde met de stelling van Pythagoras
+zijde3 = (zijde1**2 + zijde2**2)**0.5
+
+# Print de lengte van de derde zijde
+print("De lengte van de schuine zijde is:", zijde3)
 
